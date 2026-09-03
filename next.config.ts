4dd85b0cb235162421b2ec6257e1bd9fb2a96bdd
hassbro <nassaah@gmail.com>
2026-09-03 18:02:31 +0500

    update routing and config

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/",
-        // You can check a specific query parameter or header, 
-        // or ensure it only redirects if it's a direct visit without a launch flag
-        has: [
-          {
-            type: "query",
-            key: "mode",
-            value: "admin", // Only redirects if ?mode=admin is present, or vice-versa
-          },
-        ],
-        destination: "/admin",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
